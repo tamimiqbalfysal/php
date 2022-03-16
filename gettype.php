9cbc5e3eadfc899b7420b1da29c88c82af5fe221
tamimiqbalfysal <tamimiqbal.fysal@gmail.com>
2022-03-16 20:56:28 +0600

    init

---
+++ gettype.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewpoint"
+          content = "width=device-width">
+    <meta http-equiv="X-UA-Compatible">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+$name = 'fysal';
+
+$age= 27; //not all variable need colon 
+
+$ismale= true;
+
+$height= 5.7; 
+
+$salary=null;
+
+echo $name.'<br>';
+echo $age. '<br>';
+echo $ismale. '<br>';
+echo $height. '<br>';
+echo $salary. '<br>'; 
+
+echo gettype ($name). '<br>';
+echo gettype ($age). '<br>';
+echo gettype ($ismale). '<br>';
+echo gettype ($height). '<br>';
+echo gettype ($salary). '<br>'; 
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+</body>
+</html>
+
+
+
+
+    
+
+
+
